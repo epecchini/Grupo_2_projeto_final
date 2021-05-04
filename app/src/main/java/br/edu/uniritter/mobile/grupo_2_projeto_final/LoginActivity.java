b1294e704e8ccffa4094ebeb8edf139d6d8faf2e
Eliseo Pecchini <epecchini76@gmail.com>
2021-05-04 20:14:22 -0300

    Adicionando LoginActivity

---
+++ app/src/main/java/br/edu/uniritter/mobile/grupo_2_projeto_final/LoginActivity.java
@@ -0,0 +1,14 @@
+package br.edu.uniritter.mobile.grupo_2_projeto_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
