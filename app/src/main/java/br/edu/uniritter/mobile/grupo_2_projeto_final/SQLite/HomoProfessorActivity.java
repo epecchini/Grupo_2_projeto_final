270a897f3a8cc2455289980928b3ef2f33dd2be5
Alvino Rizzi <alvinorizzi@hotmail.com>
2021-05-14 18:20:04 -0300

    Commit teste

---
+++ app/src/main/java/br/edu/uniritter/mobile/grupo_2_projeto_final/SQLite/HomoProfessorActivity.java
@@ -0,0 +1,16 @@
+package br.edu.uniritter.mobile.grupo_2_projeto_final.SQLite;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.edu.uniritter.mobile.grupo_2_projeto_final.R;
+
+public class HomoProfessorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_professor);
+    }
+
+}
